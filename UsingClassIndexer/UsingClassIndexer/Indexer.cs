3b5f66d308a20a6be39ce059a463e6e1bce918e9
henrique-pignatari <65184453+henrique-pignatari@users.noreply.github.com>
2020-11-08 15:27:15 -0300

    Add files via upload
    This commit has the whole code that I intended to create using extensions methods for the object type, so any object can easily use the indexer.

---
+++ UsingClassIndexer/UsingClassIndexer/Indexer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingClassIndexer
+{
+    public static class Indexer
+    {
+        public static object GetPropertyOrMethod(this object classToGet, string name)
+        {
+            var property = GetProperty(classToGet, name);
+            var method = GetMethod(classToGet, name);
+
+            if (property != null)
+            {
+                return property.GetValue(classToGet);
+            }
+            else if (method != null)
+            {
+                return method;
+            }
+            return null;
+        }
+
+        public static PropertyInfo GetProperty(object classToGet, string name)
+        {
+            return classToGet.GetType().GetProperty(name);
+        }
+
+        public static MethodInfo GetMethod(object classToGet, string name)
+        {
+            return classToGet.GetType().GetMethod(name);
+        }
+
+        public static void SetProperty(this object classToGet, string name, params object[] args)
+        {
+            PropertyInfo property = GetProperty(classToGet, name);
+
+            if (property != null)
+            {
+                MethodInfo setMethod = property.GetSetMethod();
+
+                if (setMethod != null)
+                {
+                    setMethod.Invoke(classToGet, args);
+                }
+                else throw new NullReferenceException();
+            }
+            else throw new NullReferenceException();
+        }
+    }
+}
---
+++ UsingClassIndexer/UsingClassIndexer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingClassIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassWithIndexers indexedClass = new ClassWithIndexers();
+
+            string name = "property";
+            indexedClass[name] = 50;
+
+            Console.WriteLine(indexedClass[name]);
+
+            name = "Method";
+
+            MethodInfo method = indexedClass[name] as MethodInfo;
+            
+            Console.WriteLine(method.Invoke(indexedClass, new object[] { }));
+
+            Console.ReadLine();
+        }
+
+        public class ClassWithIndexers
+        {
+            public int property { get; set; }
+            private int property2 { get; set; }
+
+            public ClassWithIndexers()
+            {
+                property = 45;
+                property2 = 93;
+            }
+
+            public int Method()
+            {
+                return 60;
+            }
+
+            public object this[string name]
+            {
+                get
+                {                   
+                    var propertyOrMethod = this.GetPropertyOrMethod(name);
+
+                    if (propertyOrMethod != null)
+                    {
+                        return propertyOrMethod;
+                    }
+                    else throw new NullReferenceException();
+                }
+
+                set
+                {
+                    this.SetProperty(name, value);
+                }
+            }
+        }
+    }
+}
